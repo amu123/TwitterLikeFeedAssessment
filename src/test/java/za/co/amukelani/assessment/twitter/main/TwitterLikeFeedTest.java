74c80cedb6baac1aebef1108419050f13a2689b8
Amukelani Mathye <AmukelaniM5@discovery.co.za>
2019-07-05 04:38:44 +0200

    update unit test for main

---
+++ src/test/java/za/co/amukelani/assessment/twitter/main/TwitterLikeFeedTest.java
@@ -0,0 +1,15 @@
+package za.co.amukelani.assessment.twitter.main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TwitterLikeFeedTest {
+
+    @Test
+    public void test(){
+        String[] args = {"one"};
+        TwitterLikeFeed.main(args);
+    }
+
+}
